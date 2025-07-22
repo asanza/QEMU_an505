c28609ac84e21412b65581435b96352732f34e56
Diego Asanza <f.asanza@gmail.com>
2025-07-22 07:07:50 +0200

    initial commit
    Signed-off-by: Diego Asanza <f.asanza@gmail.com>

--- non_secure/main_ns.c
+++ non_secure/main_ns.c
@@ -1,13 +1,16 @@
 #include "logPrint.h"
 #include "uart.h"
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-extern int sec_sum(void);
+extern int sec_sum(int idx, int m);
 
 /* Non Secure main() */
 int main(void) {
-    sec_sum();
+    int i = 0;
 
-    while(1);
-    return 0;
+    i = sec_sum(i, 1);
+    printf("Hello from non-secure world: %d\n", i);
+    exit(0);
 }
